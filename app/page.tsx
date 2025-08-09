97f93ec56d3abeb3786f4ed33fe563d20fd985e4
ceokobec-hue <ceokobec@gmail.com>
2025-08-09 09:37:49 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to LectureHub</h1>
+      <p>이 페이지는 Vercel에서 정상 배포되는지 확인하는 테스트 페이지입니다.</p>
+    </main>
+  );
+}
